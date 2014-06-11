17f07af9c85f3dac229adea96c0a12debf28db40
wktk <wktk30@gmail.com>
2014-06-11 18:49:56 +0900

    Support changing profile images

--- worker.rb
+++ worker.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'tempfile'
 require 'twitter'
 
 options = {
@@ -13,12 +15,23 @@ user = twitter.current_user
 
 stream.user do |tweet|
   next unless tweet.is_a?(Twitter::Tweet)
-  next unless match_data = tweet.text.match(/(.+)[（\(][@＠]#{user.screen_name}[\)）]/i)
 
-  new_name = match_data[1]
-  updated_profile = twitter.update_profile(name: new_name)
-  twitter.update(
-    "@#{tweet.user.screen_name} #{updated_profile.name.gsub(/[@＠]/, '@ ')}になりました.",
-    in_reply_to_status_id: tweet.id
-  )
+  if match_data = tweet.text.match(/(.+)[（\(][@＠]#{user.screen_name}[\)）]/i)
+    new_name = match_data[1]
+    updated_profile = twitter.update_profile(name: new_name)
+    twitter.update(
+      "@#{tweet.user.screen_name} #{updated_profile.name.gsub(/[@＠]/, '@ ')}になりました.",
+      in_reply_to_status_id: tweet.id
+    )
+  elsif tweet.text.match(/[@＠]#{user.screen_name}\s+(?:icon|アイコン)/i)
+    next unless tweet.media?
+
+    image = Tempfile.new('')
+    image.write(open(tweet.media.first.media_url_https.to_s + ':large').read)
+    image.close; image.open
+
+    twitter.update_profile_image(image)
+    image.close; image.open
+    twitter.update_with_media("@#{tweet.user.screen_name} アイコンを更新しました: ", image) rescue nil
+  end
 end
