03d185cb21dbd84901030c5d27c797f341481b52
wktk <wktk30@gmail.com>
2014-06-14 08:33:06 +0900

    Temp

--- update_name.rb
+++ update_name.rb
@@ -16,44 +16,54 @@ class UpdateName
 
   def run(*mode)
     @mode = mode
-    stream.user do |object|
-      process_streaming_object(object)
-    end
+    stream.user { |object| process_streaming_object(object) }
   end
 
   def process_streaming_object(object)
     case object
+    when Twitter::Streaming::FriendList
+      warn 'Connected to stream'
     when Twitter::Tweet
-      update_name(object)
-      update_icon(object)
+      update_name(object) if mode.include?(:name)
+      update_icon(object) if mode.include?(:icon)
     end
   rescue => e
     warn "#{e.class}: #{e.message}"
   end
 
+  # @param [Twitter::Tweet] tweet
+  # @return [void]
   def update_name(tweet)
-    return unless mode.include?(:name)
-    return unless match_data = object.text.match(/(.+)[\(（][@＠]#{user.screen_name}[\)）]/i)
+    return unless match_data = tweet.text.match(/(.+)[\(（][@＠]#{user.screen_name}[\)）]/i)
     new_name = match_data[1]
     updated_profile = twitter.update_profile(name: new_name)
-    message = "@#{tweet.user.screen_name} #{updated_profile.name.gsub(/[@＠]/, '@ ')}になりました.",
+    message = "@#{tweet.user.screen_name} #{updated_profile.name.gsub(/[@＠]/, '@ ')}になりました."
     warn message
     twitter.update(message, in_reply_to_status_id: tweet.id)
   end
 
+  # @param [Twitter::Tweet] tweet
+  # @return [void]
   def update_icon(tweet)
-    return unless mode.include?(:icon) && tweet.media?
-    return unless tweet.text =~ /[@＠]#{user.screen_name}\s+(?:icon|アイコン)/i
+    return unless tweet.media? && tweet.text =~ /(?:icon|アイコン)/i
+    return unless tweet.text =~ /[@＠]#{user.screen_name}/i
 
-    image = Tempfile.new(['update-icon', tweet.media.first.media_url_https.extname])
-    image.write(open(tweet.media.first.media_url_https.to_s + ':large').read)
-    image.close
-
+    image = temp_download(tweet.media.first.media_url_https.to_s + ':large')
     twitter.update_profile_image(File.open(image.path))
     message = "@#{tweet.user.screen_name} アイコンを更新しました"
     warn message
-    twitter.update_with_media(message, File.open(image.path))
-    image.close!
+    twitter.update_with_media(message, File.open(image.path), in_reply_to_status_id: tweet.id)
+  end
+
+  private
+
+  # @param [String] uri
+  # @return [Tempfile]
+  def temp_download(uri)
+    file = Tempfile.open('update-icon')
+    file.write(OpenURI.open_uri(uri).read)
+    file.close
+    file
   end
 
 end
--- worker.rb
+++ worker.rb
@@ -8,4 +8,4 @@ options = {
 }
 
 update_name = UpdateName.new(options)
-update_name.run(:name, :image)
+update_name.run(:name, :icon)
